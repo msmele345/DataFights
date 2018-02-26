c485d3b1a97de134b114a4c8a272dbb0bd6ffc65
Mitch Mele <mitchmele@gmail.com>
2018-02-26 09:18:33 -0600

    Add user auth and new user feature.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
 
+    ##static home page
+    root 'static_pages#home'
 
-  root 'static_pages#home'
+    ##create a new user
+    resources :users, only: [:new, :create, :show]
+
+    ##login routes
+    get '/login', to: 'sessions#new'
+    post '/login', to: 'sessions#create'
+    get '/logout', to: 'sessions#destroy'
 
 
 
