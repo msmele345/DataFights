e809de83fae61826182ca4a64cfa5b7e1db6c4db
Mitch Mele <mitchmele@gmail.com>
2018-03-14 08:23:56 -0500

    Add edit template, update users controller eith edit/update methods.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,6 +36,19 @@ class UsersController < ApplicationController
    end
   end
 
+  def edit 
+    @user = User.find(params[:id])
+  end 
+
+  def update 
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to edit_user_path(current_user)      
+    else 
+      @errors = @user.errors.full_messages
+      render :edit
+  end 
+
   private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password)
